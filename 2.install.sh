40cc3b6970b6d9282a5aa4974f0c38390399d762
khl1215 <khl.user15@gmail.com>
2025-03-12 02:04:44 +0700

    Add files via upload

---
+++ 2.install.sh
@@ -0,0 +1,28 @@
+for ((i = 5; i >= 1; i--)); do
+    sleep 1
+    echo "Install Parameter $i. Tekan ctrl+c untuk membatalkan"
+done
+
+mkdir /root/db
+cp config.bson /root/db
+cp config.metadata.json /root/db
+cp permissions.bson /root/db
+cp permissions.metadata.json /root/db
+cp presets.bson /root/db
+cp presets.metadata.json /root/db
+cp provisions.bson /root/db
+cp provisions.metadata.json /root/db
+cp tasks.bson /root/db
+cp tasks.metadata.json /root/db
+cp users.bson /root/db
+cp users.metadata.json /root/db
+cp virtualParameters.bson /root/db
+cp virtualParameters.metadata.json /root/db
+cd 
+sudo mongodump --db=genieacs --out genieacs-backup
+mongorestore --db genieacs --drop /root/db
+echo -e "${GREEN}============================================================================${NC}"
+echo -e "${GREEN}=================== VIRTUAL PARAMETER BERHASIL DI INSTALL. =================${NC}"
+echo -e "${GREEN}========== GenieACS UI akses port 3000. : http://$local_ip:3000 ============${NC}"
+echo -e "${GREEN}=========================== DONE INSTALLATON ===============================${NC}"
+echo -e "${GREEN}============================================================================${NC}"
